16878bcc034f5cb55304eddc4b4e2cf813aafe2e
jariseon <jari.kleimola@outlook.com>
2018-03-29 20:50:06 +0300

    namespace support

--- build/encode-wasm.js
+++ build/encode-wasm.js
@@ -1,13 +1,17 @@
+if (process.argv.length != 3) {
+  console.log("usage: node encode-wasm.js mymodule.wasm");
+	return;
+}
+
+let wasmName = process.argv[2];
+let name = wasmName.substr(0, wasmName.length - 5).toUpperCase();
+
 // thanks to Steven Yi / Csound
 //
 fs = require('fs');
-let wasmData = fs.readFileSync("dx7.wasm");
+let wasmData = fs.readFileSync(wasmName);
 let wasmStr = wasmData.join(",");
-let wasmOut = "AudioWorkletGlobalScope.WAM = { ENVIRONMENT: 'WEB' }\n";
-wasmOut += "AudioWorkletGlobalScope.WAM.wasmBinary = new Uint8Array([" + wasmStr + "]);";
-fs.writeFileSync("dx7.wasm.js", wasmOut);
-
-// later we will possibly switch to base64
-// as suggested by Stephane Letz / Faust
-// let base64 = wasmData.toString("base64");
-// fs.writeFileSync("dx7.base64.js", base64);
+let wasmOut = "AudioWorkletGlobalScope.WAM = AudioWorkletGlobalScope.WAM || {}\n";
+wasmOut += "AudioWorkletGlobalScope.WAM." + name + " = { ENVIRONMENT: 'WEB' }\n";
+wasmOut += "AudioWorkletGlobalScope.WAM." + name + ".wasmBinary = new Uint8Array([" + wasmStr + "]);";
+fs.writeFileSync(wasmName + ".js", wasmOut);
